157fb645a1f8b15e1d10d25f973ff465ea2a07a3
J K <jkim65@buffalo.edu>
2020-10-09 04:11:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
    FROM authors
    JOIN subgenres
-   ON authors.
+   ON authors.id = subgenres.id
    "
 end
 
