18fde9d7baef21c1f89c0eddfc16c24904c758d2
J K <jkim65@buffalo.edu>
2020-10-09 04:28:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name
-   FROM
-   DESC"
+  "SELECT characters.name, COUNT(*)
+   FROM 
+   ORDER BY COUNT(*) DESC"
 end
