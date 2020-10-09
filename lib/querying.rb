128af9e9801b6487474c6806692990c8d40e18b5
J K <jkim65@buffalo.edu>
2020-10-09 04:36:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,6 +43,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(book_id)
    FROM characters
-   INNER JOIN character_books 
-   GROUP BY ORDER BY COUNT(*) DESC"
+   INNER JOIN character_books
+   ON characters.id = character_books.
+   GROUP BY ORDER BY COUNT(book_id) DESC"
 end
