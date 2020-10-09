fdae0edaf9f88470d8635a30d512a1d3e2814fc9
J K <jkim65@buffalo.edu>
2020-10-09 04:20:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
    ON series.id = books.series_id
    JOIN character_books
    ON character_books.character_id = characters.id WHERE characters.species = 'human'
-   GROUP BY DESC LIMIT 1
+   GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1
   "
 end
 
